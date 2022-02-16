7e35ba517e7a93f7dabd5b6be252cfb781023baf
kajafxluvels <elsapriliashawty@gmail.com>
2022-02-16 17:57:20 +0700

    Update Primary.php

--- web-admin/app/Controllers/Primary.php
+++ web-admin/app/Controllers/Primary.php
@@ -3,12 +3,15 @@
 namespace App\Controllers;
 
 use App\Models\absensi_model;
+use App\Models\User_model;
+use Dompdf\Dompdf;
 
 class Primary extends BaseController
 {
     public function home()
     {
-        return view('admin-dasboard/index');
+        $session = session();
+        return view('admin-dasboard/halamanutama');
     }
     public function tables()
     {
@@ -22,17 +25,28 @@ class Primary extends BaseController
         return view('admin-dasboard/tables', $data);
     }
     public function rekap_absen()
+    {       
+            $absensimodel = new absensi_model();
+        
+            $rekapabsen = $absensimodel->findAll();
+            $data = [
+                'rekap_absensi' => $rekapabsen
+            ];
+            $dompdf = new Dompdf();
+            $dompdf->loadHtml(view('admin-dasboard/tabelrekap',$data));
+            $dompdf->setPaper('A4','portrait');
+            $dompdf->render();
+            $dompdf->stream('Daftar Absensi');
+
+            return redirect()->to(base_url('admin-dasboard/tabel'));
+
+    }
+    public function jumlahabsen()
     {
         $absensimodel = new absensi_model();
-        $bulan = $this->request->getPost('month');
-
-        $rekapabsen = $absensimodel->where('MONTHNAME(tanggal)', $bulan)->first();
-
-        $data = [
-            'rekap_absensi' => $rekapabsen
-        ];
-        dd($data);
-
-        return view('admin-dasboard/tabelrekap', $data);
+        $result = $absensimodel->select('sum(NIP) as jml')->first();
+        $data['sum'] = $result['jml'];
+        
+        return view('admin-dasboard/halamanutama', $data);
     }
 }
